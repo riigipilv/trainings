bf3233d31f53e2dd5b9901e8eebf7f8dd53a5232
Pelle Jakovits <jakovits@ut.ee>
2021-11-01 21:18:49 +0200

    Update monitor.sh

--- abc_est/7/monitor.sh
+++ abc_est/7/monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl cp how_many_pods.sh client:/how_many_pods.sh
+k cp how_many_pods.sh client:/how_many_pods.sh
 
 
 
@@ -7,14 +7,14 @@ servers=0
 while [ $servers -lt 5 ]
 do
    echo "###########Web Requests#############"
-   kubectl exec -it client /how_many_pods.sh > ./result.txt
+   k exec -it client /how_many_pods.sh > ./result.txt
    echo "50 requests divided on these pods:"
    cat ./result.txt
    servers=`cat ./result.txt | wc -l`
    echo "-----------Pods---------------------"
-   kubectl get pods 
+   k get pods 
    echo "-----------Endpoints IPs------------"
-   kubectl describe ep liveandready | grep "Addresses:\|NotReadyAddresses:"
+   k describe ep liveandready | grep "Addresses:\|NotReadyAddresses:"
    if [ $servers -lt 5 ]
    then
      sleep 10
